502108aef8acb4ca3e089bd5ab6bcc06577eb7e2
AngelinaEvs <57727402+AngelinaEvs@users.noreply.github.com>
2020-10-22 19:23:35 +0300

    HW6 (RecyclerView)

---
+++ RecyclerViewProject/app/src/main/java/ru/kpfu/stud/recyclerviewproject/Planet.kt
@@ -0,0 +1,10 @@
+package ru.kpfu.stud.recyclerviewproject
+
+class Planet(
+    val name: String,
+    val id: Int,
+    val satellites: String,
+    val structure: String,
+    val info: String,
+    val photo: Int
+)
---
+++ RecyclerViewProject/app/src/main/java/ru/kpfu/stud/recyclerviewproject/PlanetsObject.kt
@@ -0,0 +1,5 @@
+package ru.kpfu.stud.recyclerviewproject
+
+object PlanetsObject {
+    var planetsObject = arrayListOf<Planet>()
+}
